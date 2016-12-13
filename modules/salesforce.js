0adf56626de5e9acef450e3d8192fbff70f7f9a3
Glenn Coppens <glenn.coppens@gmail.com>
2016-12-13 17:02:12 +0100

    fix method name in export

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -178,4 +178,4 @@ exports.findProperties = findProperties;
 exports.findPropertiesByCategory = findPropertiesByCategory;
 exports.findPriceChanges = findPriceChanges;
 exports.createCase = createCase;
-exports.findBroker = findBroker;
+exports.findBrokers = findBrokers;
